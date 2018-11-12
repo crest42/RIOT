88504ebefa5062f1c033ea6a789e640904abfbf7
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-11-12 18:33:02 +0100

    tests-crypto/test-crypto.h: update api to const input buffers
    Input buffers are not modified, so can be declared const arguments.
    This will allow using `const` inputs for `modes/ccm` tests.

--- tests/unittests/tests-crypto/tests-crypto.h
+++ tests/unittests/tests-crypto/tests-crypto.h
@@ -46,7 +46,7 @@ Test *tests_crypto_helper_tests(void);
  */
 Test *tests_crypto_chacha_tests(void);
 
-static inline int compare(uint8_t *a, uint8_t *b, uint8_t len)
+static inline int compare(const uint8_t *a, const uint8_t *b, uint8_t len)
 {
     int result = 1;
 
