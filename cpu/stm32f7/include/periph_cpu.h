2045eb151eaad5d5f42c24c8bc03fc584aac7b6a
Robin <robin@chilio.net>
2019-02-18 16:05:00 +0100

    stm32_eth: Move periph configuration struct to stm32_common

--- cpu/stm32f7/include/periph_cpu.h
+++ cpu/stm32f7/include/periph_cpu.h
@@ -53,30 +53,6 @@ enum {
     PORT_K = 10             /**< port K */
 };
 
-/**
- * @brief   Ethernet Peripheral configuration
- */
-typedef struct {
-    enum {
-        MII = 18,                       /**< Configuration for MII */
-        RMII = 9,                       /**< Configuration for RMII */
-        SMI = 2,                        /**< Configuration for SMI */
-    } mode;                             /**< Select configuration mode */
-    enum {
-        ETH_SPEED_10T_HD = 0x0000,
-        ETH_SPEED_10T_FD = 0x0100,
-        ETH_SPEED_100TX_HD = 0x2000,
-        ETH_SPEED_100TX_FD = 0x2100,
-    } speed;                            /**< Speed selection */
-    uint8_t dma;                        /**< Locical CMA Descriptor used for TX */
-    uint8_t dma_chan;                   /**< DMA channel used for TX */
-    char mac[6];                        /**< Et hernet MAC address */
-    char phy_addr;                      /**< PHY address */
-    gpio_t pins[];                      /**< Pins to use. MII requires 18 pins,
-                                             RMII 9 and SMI 9. Not all speeds are
-                                             supported by all modes. */
-} eth_conf_t;
-
 #ifdef __cplusplus
 }
 #endif
